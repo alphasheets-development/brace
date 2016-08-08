ab6d0da9f475a4fae8eaa387e52ae4eee2e7c69d
anand <asrini@mit.edu>
2016-08-07 21:55:49 -0700

    alphasheets build

--- index.js
+++ index.js
@@ -4124,7 +4124,7 @@ var KeyBinding = function(editor) {
                 e.code == "ArrowRight"
                 || e.code == "ArrowLeft"
                 || e.code == "ArrowUp"
-                || e.code == "ArrowDown"]
+                || e.code == "ArrowDown"
                 || (e.code == "KeyA" && e.ctrlKey)
             );
         }
